aa2a3f166ace311c84de63c54406f952148c9200
kiricon <kiricon@live.com>
2017-02-18 10:10:15 -0700

    You can now kinda step through the directory.

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,27 @@
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.open('POST', '/directory/', true);
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4) {
-        if(xmlhttp.status == 200) {
-            var obj = JSON.parse(xmlhttp.responseText);
-            console.log(obj);
-         }
-    }
-};
-var json = JSON.stringify({"Directory": ""});
-xmlhttp.send(json);
+function getDirectory(dir) {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', '/directory/', true);
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4) {
+            if(xmlhttp.status == 200) {
+                let obj = JSON.parse(xmlhttp.responseText);
+                let directory = document.getElementById('directory');
+                directory.innerHTML = '';
+
+                for(let i = 0; i < obj.length; i++){
+                    let item = document.createElement('div');
+                    item.innerHTML = obj[i];
+                    item.addEventListener('click', () => {
+                        getDirectory(obj[i]);
+                    });
+                    directory.appendChild(item);
+                }
+
+            }
+        }
+    };
+    let json = JSON.stringify({"Directory": dir});
+    xmlhttp.send(json);
+}
+
+getDirectory('');
