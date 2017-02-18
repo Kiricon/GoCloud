0b20ac7e8eee8a02be1e3376ee41dc07be89c98c
kiricon <kiricon@live.com>
2017-02-18 12:13:42 -0700

    Broke out rendering code much more.

--- public/js/explorer/explorer.js
+++ public/js/explorer/explorer.js
@@ -18,27 +18,38 @@ class Explorer {
         let self = this;
         let xmlhttp = new XMLHttpRequest();
         xmlhttp.open('POST', '/directory/', true);
-        xmlhttp.onreadystatechange = function() {
+        xmlhttp.onreadystatechange = () => {
             if (xmlhttp.readyState == 4) {
                 if(xmlhttp.status == 200) {
                     let obj = JSON.parse(xmlhttp.responseText);
-                    let directory = document.getElementById('directory');
-                    directory.innerHTML = '';
-
-                    for(let i = 0; i < obj.length; i++) {
-                        let item = document.createElement('div');
-                        item.innerHTML = obj[i];
-                        item.addEventListener('click', () => {
-                            self.getDirectory(obj[i]);
-                        });
-                        directory.appendChild(item);
-                    }
+                    self.buildItems(obj);
                 }
             }
         };
         let json = JSON.stringify({'Directory': this.currentDir + dir});
         xmlhttp.send(json);
         this.currentDir += dir+'/';
+    }
+
+
+    /**
+     * Method to build HTML elements from
+     * returned list of files and directories
+     * @param {String[]} obj - List of files and directories
+     */
+    buildItems(obj) {
+        let self = this;
+        let directory = document.getElementById('directory');
+        directory.innerHTML = '';
+
+        for(let i = 0; i < obj.length; i++) {
+            let item = document.createElement('div');
+            item.innerHTML = obj[i];
+            item.addEventListener('click', () => {
+                self.getDirectory(obj[i]);
+            });
+            directory.appendChild(item);
+        }
     }
 }
 
